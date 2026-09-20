17ec526387e7d4e74672241155d547d9c2e1d454
fit-com <nsufitcommunity@proton.me>
2026-09-21 04:10:06 +0700

    Implement volume calculation for spherical layers
    Added functions to calculate the volume of a spherical mantle and core based on the radius input.

--- lab1/1/main.cpp
+++ lab1/1/main.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+#define pi 3.14
+
+double volume(double radius) {
+    return 4.0 / 3.0 * pi * radius * radius * radius;
+}
+
 int main() {
-  //Ваш код
-  return 0;
+    int radius;
+    scanf("%d", &radius);
+
+    double mantle_radius = 0.995 * radius;
+    double core_radius = 0.545 * radius;
+
+    printf("%llu\n", (unsigned long long)(volume(mantle_radius) - volume(core_radius)));
+    return 0;
 }
